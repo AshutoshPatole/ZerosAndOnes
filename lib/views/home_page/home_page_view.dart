52dc145cb47933003e01a6ba86476c342aa28fd0
AshutoshPatole <apatole479@gmail.com>
2021-10-17 19:55:45 +0530

    Add coupon widget

--- lib/views/home_page/home_page_view.dart
+++ lib/views/home_page/home_page_view.dart
@@ -32,32 +32,25 @@ class HomePageView extends StatelessWidget {
                         child: CircularProgressIndicator(),
                       )
                     : viewModel.isServiceableDistance
-                        ? Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 12),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                HomeHeader(address: viewModel.address),
-                                SizedBox(
-                                  height: size.height * 0.05,
-                                ),
-                                const Text(
-                                  "Categories",
-                                  style: AppTheme.title,
-                                ),
-                                SizedBox(
-                                  height: size.height * 0.3,
-                                  child: ListView.builder(
-                                    shrinkWrap: true,
-                                    scrollDirection: Axis.horizontal,
-                                    physics: const BouncingScrollPhysics(),
-                                    itemCount: mockFood.length,
-                                    itemBuilder: (context, index) {
-                                      return Container(
-                                        width: size.width * 0.3,
-                                        height: size.height * 0.1,
-                                        margin: const EdgeInsets.all(10),
-                                        padding: const EdgeInsets.all(10),
+                        ? SingleChildScrollView(
+                            physics: const BouncingScrollPhysics(),
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 12),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  HomeHeader(address: viewModel.address),
+                                  SizedBox(
+                                    height: size.height * 0.05,
+                                  ),
+                                  Stack(
+                                    clipBehavior: Clip.none,
+                                    children: [
+                                      Container(
+                                        width: size.width,
+                                        height: size.height * 0.2,
+                                        padding: const EdgeInsets.all(20),
                                         decoration: BoxDecoration(
                                           color: AppTheme.primaryColor,
                                           borderRadius:
@@ -66,43 +59,140 @@ class HomePageView extends StatelessWidget {
                                             BoxShadow(
                                               color: AppTheme.primaryColor
                                                   .withOpacity(0.75),
-                                              spreadRadius: 1,
+                                              spreadRadius: 0,
                                               blurRadius: 5,
-                                              offset: const Offset(2, 4.5),
+                                              offset: const Offset(2, 4),
                                             )
                                           ],
                                         ),
                                         child: Column(
-                                          children: [
-                                            SizedBox(
-                                              height: size.height * 0.025,
-                                            ),
-                                            Image.asset(
-                                                mockFood[index].imageLink),
-                                            SizedBox(
-                                              height: size.height * 0.01,
-                                            ),
-                                            Text(mockFood[index].name),
-                                            Container(
-                                              width: size.width * 0.08,
-                                              height: size.height * 0.08,
-                                              decoration: const BoxDecoration(
-                                                shape: BoxShape.circle,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: const [
+                                            Text(
+                                              "25% Off",
+                                              style: TextStyle(
                                                 color: Colors.white,
+                                                fontSize: 30,
+                                                fontWeight: FontWeight.bold,
                                               ),
-                                              child: const Icon(
-                                                CupertinoIcons.right_chevron,
-                                                color: Colors.black,
-                                                size: 16,
+                                            ),
+                                            Text(
+                                              "Apply APPSPL25 coupon",
+                                              style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 18,
+                                                fontWeight: FontWeight.w600,
                                               ),
-                                            )
+                                            ),
                                           ],
                                         ),
-                                      );
-                                    },
+                                      ),
+                                      Positioned(
+                                        right: size.width * 0.05,
+                                        top: size.height * 0.05,
+                                        child: Container(
+                                          width: size.width * 0.4,
+                                          height: size.height * 0.4,
+                                          decoration: BoxDecoration(
+                                            shape: BoxShape.circle,
+                                            color:
+                                                Colors.white.withOpacity(0.2),
+                                          ),
+                                        ),
+                                      ),
+                                      Positioned(
+                                        left: size.width * 0.05,
+                                        bottom: size.height * 0.05,
+                                        child: Container(
+                                          width: size.width * 0.4,
+                                          height: size.height * 0.4,
+                                          decoration: BoxDecoration(
+                                            shape: BoxShape.circle,
+                                            color:
+                                                Colors.white.withOpacity(0.2),
+                                          ),
+                                        ),
+                                      ),
+                                      Positioned(
+                                        right: -50,
+                                        top: -65,
+                                        child: Image.asset(
+                                          'assets/images/nice.png',
+                                          width: 200,
+                                          height: 200,
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                ),
-                              ],
+                                  SizedBox(
+                                    height: size.height * 0.05,
+                                  ),
+                                  const Text(
+                                    "Categories",
+                                    style: AppTheme.title,
+                                  ),
+                                  SizedBox(
+                                    height: size.height * 0.3,
+                                    child: ListView.builder(
+                                      shrinkWrap: true,
+                                      scrollDirection: Axis.horizontal,
+                                      physics: const BouncingScrollPhysics(),
+                                      itemCount: mockFood.length,
+                                      itemBuilder: (context, index) {
+                                        return Container(
+                                          width: size.width * 0.3,
+                                          height: size.height * 0.1,
+                                          margin: const EdgeInsets.all(10),
+                                          padding: const EdgeInsets.all(10),
+                                          decoration: BoxDecoration(
+                                            color: AppTheme.primaryColor,
+                                            borderRadius:
+                                                BorderRadius.circular(20.0),
+                                            boxShadow: [
+                                              BoxShadow(
+                                                color: AppTheme.primaryColor
+                                                    .withOpacity(0.75),
+                                                spreadRadius: 1,
+                                                blurRadius: 5,
+                                                offset: const Offset(2, 4.5),
+                                              )
+                                            ],
+                                          ),
+                                          child: Column(
+                                            children: [
+                                              SizedBox(
+                                                height: size.height * 0.025,
+                                              ),
+                                              Image.asset(
+                                                  mockFood[index].imageLink),
+                                              SizedBox(
+                                                height: size.height * 0.01,
+                                              ),
+                                              Text(mockFood[index].name),
+                                              Container(
+                                                width: size.width * 0.08,
+                                                height: size.height * 0.08,
+                                                decoration: const BoxDecoration(
+                                                  shape: BoxShape.circle,
+                                                  color: Colors.white,
+                                                ),
+                                                child: const Icon(
+                                                  CupertinoIcons.right_chevron,
+                                                  color: Colors.black,
+                                                  size: 16,
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                        );
+                                      },
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
                           )
                         : const NotServiceableLocation();
