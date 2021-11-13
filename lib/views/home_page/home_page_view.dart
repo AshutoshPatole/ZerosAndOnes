9d081699f0e38799687249cbee3d63f5c9ca2844
AshutoshPatole <apatole479@gmail.com>
2021-11-13 18:25:24 +0530

    Add firebase performance sdk

--- lib/views/home_page/home_page_view.dart
+++ lib/views/home_page/home_page_view.dart
@@ -1,7 +1,5 @@
-import 'package:animations/animations.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:stacked/stacked.dart';
 import 'package:zerosandones/core/models/mock_data.dart';
 import 'package:zerosandones/core/models/user_location.dart';
