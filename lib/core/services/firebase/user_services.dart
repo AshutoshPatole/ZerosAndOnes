8a9a41b2c6f1b9ecd372d5cd733325928b330b42
AshutoshPatole <apatole479@gmail.com>
2021-11-24 20:17:47 +0530

    incomplete things

--- lib/core/services/firebase/user_services.dart
+++ lib/core/services/firebase/user_services.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 import 'package:zerosandones/core/logger.dart';
 
@@ -27,20 +26,38 @@ class UserService {
       required String itemName}) async {
     try {
       if (_auth.currentUser?.uid != null) {
-        CollectionReference collectionReference = database
-            .collection("users")
-            .doc(_auth.currentUser?.uid)
-            .collection("cart");
-        String docId = collectionReference.doc().id;
-        await collectionReference.doc(docId).set({
-          "itemId": itemId,
-          "price": price,
-          "itemPhoto": itemPhoto,
-          "quantity": quantity,
-          "id": docId,
-          "itemName": itemName,
-        });
-        return true;
+        var existsDoc = await database
+            .collection("users/${_auth.currentUser?.uid}/cart")
+            .where(
+              'itemId',
+              isEqualTo: itemId,
+            )
+            .get();
+        if (existsDoc.docs[0].exists) {
+          var docId = existsDoc.docs[0].reference.id;
+          CollectionReference updateRef = database
+              .collection("users")
+              .doc(_auth.currentUser?.uid)
+              .collection("cart");
+          // await updateRef.doc(docId).update({
+          //   'quantity':  // ! TODO Incomplete
+          // });
+        } else {
+          CollectionReference addRef = database
+              .collection("users")
+              .doc(_auth.currentUser?.uid)
+              .collection("cart");
+          String docId = addRef.doc().id;
+          await addRef.doc(docId).set({
+            "itemId": itemId,
+            "price": price,
+            "itemPhoto": itemPhoto,
+            "quantity": quantity,
+            "id": docId,
+            "itemName": itemName,
+          });
+          return true;
+        }
       } else {
         log.e("could not add product $itemId - $price");
         return false;
